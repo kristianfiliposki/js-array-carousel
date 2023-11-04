96a5b66c0238d1c67790b1fb162c024249762bde
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-04 12:14:32 +0100

    .,

--- java/main.js
+++ java/main.js
@@ -5,18 +5,18 @@ const container=document.getElementById("carousel");
 
 
 
-let foto=document.getElementsByClassName("photos")
+let foto=document.getElementsByClassName("photos");
 let foto0 =0;
 let finalfoto=foto.length -1;
 
-foto[foto0].classList.remove("invisible")
+foto[foto0].classList.remove("invisible");
 
 
 btndx.addEventListener("click",function(){
     if(foto0 < finalfoto){
-        foto[foto0].classList.add("invisible")
+        foto[foto0].classList.add("invisible");
         foto0++
-        foto[foto0].classList.remove("invisible")
+        foto[foto0].classList.remove("invisible");
     }
 })
 
